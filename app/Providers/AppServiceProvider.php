0e3846f5e346518b6ed84260a57e0f1abe316582
Zakariyae <175839414+zakariyae-C@users.noreply.github.com>
2026-04-18 17:34:23 +0100

    add HTTPS scheme enforcement in AppServiceProvider and remove debug route from web.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,6 +23,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        
+        // Use protocol https if i am not using local
+        if (config('app.env') !== 'local') {
+            URL::forceScheme('https');
+        }
+
         Gate::define('update_user', function(User $authUser, User $targetUser) : bool|Response
         {
             return $authUser->id ===  $targetUser->id
